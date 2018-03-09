0a69c8eba7dde73059bea6ad106e6732ef6d73de
Roberto <roberto.butti@gmail.com>
2018-03-09 14:16:16 +0100

    create new zombie from 2 parents ;)

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -7,6 +7,10 @@ contract ZombieFeeding is ZombieFactory {
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = ( myZombie.dna + _targetDna) / 2;
+    _createZombie("NoName", newDna);
+
   }
 
 }
