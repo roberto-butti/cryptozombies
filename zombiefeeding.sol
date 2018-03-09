ed936e768b757a70e815654ee75fdb02718ab312
Roberto <roberto.butti@gmail.com>
2018-03-09 14:09:45 +0100

    add feedAndMultiply function

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -1,8 +1,12 @@
 pragma solidity ^0.4.19;
 
-// put import statement here
 import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+  }
 
 }
