033241116df3bb73274a3b0a077c69d69215c1d9
Roberto <roberto.butti@gmail.com>
2018-03-10 14:38:02 +0100

    function feedOnKitty

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -20,15 +20,21 @@ contract KittyInterface {
 contract ZombieFeeding is ZombieFactory {
 
   address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  // Initialize kittyContract here using `ckAddress` from above
   KittyInterface kittyContract = KittyInterface(ckAddress);
-  
+
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
     _createZombie("NoName", newDna);
+  }
+
+  // define function here
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna);
   }
 
 }
