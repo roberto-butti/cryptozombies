181948dad12945d9743f2ad1f59e25036e583bc0
Roberto <roberto.butti@gmail.com>
2018-03-10 18:10:18 +0100

    manage special Kitty Genes

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -22,19 +22,24 @@ contract ZombieFeeding is ZombieFactory {
   address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
   KittyInterface kittyContract = KittyInterface(ckAddress);
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+  // Modify function definition here:
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
+    // Add an if statement here
+    if (keccak256(_species) == keccak256("kitty")) {
+      newDna = newDna - newDna % 100 + 99;
+    }
     _createZombie("NoName", newDna);
   }
 
-  // define function here
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-    feedAndMultiply(_zombieId, kittyDna);
+    // And modify function call here:
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 
 }
