7281e9905c79fe8806322da3066e7dcc01e70695
Roberto <roberto.butti@gmail.com>
2018-03-10 19:16:22 +0100

    add setKittyContractAddress

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -19,16 +19,19 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
 
-  // Modify function definition here:
+  KittyInterface kittyContract;
+
+
+  function setKittyContractAddress(address _address) external {
+    kittyContract = KittyInterface(_address);
+  }
+
   function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
-    // Add an if statement here
     if (keccak256(_species) == keccak256("kitty")) {
       newDna = newDna - newDna % 100 + 99;
     }
@@ -38,7 +41,6 @@ contract ZombieFeeding is ZombieFactory {
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-    // And modify function call here:
     feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 
