ec13e945a13f55643b4d2ae4b3ff9068ccaa4b92
Roberto <roberto.butti@gmail.com>
2018-03-14 21:14:14 +0100

    changeName and changeDna

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -4,10 +4,19 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
-  // Start here
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
   }
 
+  // Start here
+  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId){
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].name = _newName;
+  }
+
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId){
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].dna = _newDna;
+  }
 }
