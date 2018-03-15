66da1f7849a08bfd98123ed8b5e5664225444437
Roberto <roberto.butti@gmail.com>
2018-03-15 21:49:50 +0100

    function setLevelUpFee

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -4,7 +4,6 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
-  // 1. Define levelUpFee here
   uint levelUpFee = 0.001 ether;
 
   modifier aboveLevel(uint _level, uint _zombieId) {
@@ -12,12 +11,20 @@ contract ZombieHelper is ZombieFeeding {
     _;
   }
 
-  // 2. Insert levelUp function here
+  // 1. Create withdraw function here
+  function withdraw() external onlyOwner {
+    owner.transfer(this.balance);
+  }
+
+  // 2. Create setLevelUpFee function here
+  function setLevelUpFee(uint _fee) external onlyOwner {
+    levelUpFee = _fee;
+  } 
+
   function levelUp(uint _zombieId) external payable {
     require(msg.value == levelUpFee);
     zombies[_zombieId].level++;
   }
-
 
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
