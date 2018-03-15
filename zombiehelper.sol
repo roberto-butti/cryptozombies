95fb7a779f41188f02d496adcb78118ec24e8203
Roberto <roberto.butti@gmail.com>
2018-03-15 21:40:49 +0100

    levelUp is payable!!!

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -4,10 +4,20 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+  // 1. Define levelUpFee here
+  uint levelUpFee = 0.001 ether;
+
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
   }
+
+  // 2. Insert levelUp function here
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
+  }
+
 
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
@@ -21,7 +31,6 @@ contract ZombieHelper is ZombieFeeding {
 
   function getZombiesByOwner(address _owner) external view returns(uint[]) {
     uint[] memory result = new uint[](ownerZombieCount[_owner]);
-    // Start here
     uint counter = 0;
     for (uint i = 0; i < zombies.length; i++) {
       if (zombieToOwner[i] == _owner) {
