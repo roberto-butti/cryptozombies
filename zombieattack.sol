630b118fa04f14e2de54b7005061acf78964cd0d
Roberto <roberto.butti@gmail.com>
2018-03-16 09:27:15 +0100

    function attack

--- zombieattack.sol
+++ zombieattack.sol
@@ -3,12 +3,18 @@ pragma solidity ^0.4.19;
 import "./zombiehelper.sol";
 
 contract ZombieBattle is ZombieHelper {
-  // Start here
   uint randNonce = 0;
+  // Create attackVictoryProbability here
+  uint attackVictoryProbability = 70;
 
-  function randMod(uint _modulus) internal returns (uint) {
+
+  function randMod(uint _modulus) internal returns(uint) {
     randNonce++;
     return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
+  }
 
+  // Create new function here
+  function attack(uint _zombieId, uint _targetId) external {
+    
   }
 }
