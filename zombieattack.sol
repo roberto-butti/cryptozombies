8a2ac1dfb28f675770b509b7b4572e516944429c
Roberto <roberto.butti@gmail.com>
2018-03-16 09:23:20 +0100

    randMod function

--- zombieattack.sol
+++ zombieattack.sol
@@ -3,4 +3,12 @@ pragma solidity ^0.4.19;
 import "./zombiehelper.sol";
 
 contract ZombieBattle is ZombieHelper {
+  // Start here
+  uint randNonce = 0;
+
+  function randMod(uint _modulus) internal returns (uint) {
+    randNonce++;
+    return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
+
+  }
 }
