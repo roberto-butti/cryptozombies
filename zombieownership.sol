04b80bbd33c20725bac67fe8e5c0d40494bb49b5
Roberto <roberto.butti@gmail.com>
2018-03-18 19:54:44 +0100

    ERC721

--- zombieownership.sol
+++ zombieownership.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.4.19;
 
 import "./zombieattack.sol";
+import "./erc721.sol";
 
-contract ZombieOwnership is ZombieAttack {
+// Declare ERC721 inheritance here
+contract ZombieOwnership is ZombieAttack,  ERC721 {
+
 }
