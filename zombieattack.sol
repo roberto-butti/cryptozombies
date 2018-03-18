98e655f185913bc939a4a4dd8cc42ff961368530
Roberto <roberto.butti@gmail.com>
2018-03-18 18:49:26 +0100

    function attack

--- zombieattack.sol
+++ zombieattack.sol
@@ -4,17 +4,19 @@ import "./zombiehelper.sol";
 
 contract ZombieBattle is ZombieHelper {
   uint randNonce = 0;
-  // Create attackVictoryProbability here
   uint attackVictoryProbability = 70;
-
 
   function randMod(uint _modulus) internal returns(uint) {
     randNonce++;
     return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
   }
 
-  // Create new function here
-  function attack(uint _zombieId, uint _targetId) external {
-    
+  // 1. Add modifier here
+  function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+    // 2. Start function definition here
+    Zombie storage myZombie = zombies[_zombieId];
+    Zombie storage enemyZombie = zombies[_targetId];
+    uint rand = randMod(100);
+
   }
 }
