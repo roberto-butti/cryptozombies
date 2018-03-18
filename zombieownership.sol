72af78eeef96a375be3b2f4a6e4e6ca6fa876e49
Roberto <roberto.butti@gmail.com>
2018-03-18 21:00:12 +0100

    function _transfer

--- zombieownership.sol
+++ zombieownership.sol
@@ -6,13 +6,20 @@ import "./erc721.sol";
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
   function balanceOf(address _owner) public view returns (uint256 _balance) {
-    // 1. Return the number of zombies `_owner` has here
     return ownerZombieCount[_owner];
   }
 
   function ownerOf(uint256 _tokenId) public view returns (address _owner) {
-    // 2. Return the owner of `_tokenId` here
     return zombieToOwner[_tokenId];
+  }
+
+  // Define _transfer() here
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
+      ownerZombieCount[_to]++;
+      ownerZombieCount[_from]--;
+      zombieToOwner[_tokenId]=_to;
+      Transfer(_from, _to, _tokenId);
+
   }
 
   function transfer(address _to, uint256 _tokenId) public {
