1518b5b89c22cdefa253760da1abb8513de9a2e2
Roberto <roberto.butti@gmail.com>
2018-03-18 21:34:18 +0100

    SafeMath refactoring

--- zombieattack.sol
+++ zombieattack.sol
@@ -7,7 +7,8 @@ contract ZombieBattle is ZombieHelper {
   uint attackVictoryProbability = 70;
 
   function randMod(uint _modulus) internal returns(uint) {
-    randNonce++;
+    // Here's one!
+    randNonce = randNonce.add(1);
     return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
   }
 
@@ -16,13 +17,15 @@ contract ZombieBattle is ZombieHelper {
     Zombie storage enemyZombie = zombies[_targetId];
     uint rand = randMod(100);
     if (rand <= attackVictoryProbability) {
-      myZombie.winCount++;
-      myZombie.level++;
-      enemyZombie.lossCount++;
+      // Here's 3 more!
+      myZombie.winCount = myZombie.winCount.add(1);
+      myZombie.level = myZombie.level.add(1);
+      enemyZombie.lossCount = enemyZombie.lossCount.add(1);
       feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
     } else {
-      myzombie.lossCount++;
-      enemyZombie.winCount++;
+      // ...annnnd another 2!
+      myZombie.lossCount = myZombie.lossCount.add(1);
+      enemyZombie.winCount = enemyZombie.winCount.add(1);
       _triggerCooldown(myZombie);
     }
   }
