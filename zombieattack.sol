58d4cc0e2f787301c446711591510c28247a6eef
Roberto <roberto.butti@gmail.com>
2018-03-18 20:12:25 +0100

    onlyOwnerOf

--- zombieattack.sol
+++ zombieattack.sol
@@ -11,7 +11,7 @@ contract ZombieBattle is ZombieHelper {
     return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
   }
 
-  function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+  function attack(uint _zombieId, uint _targetId) external onlyOwnerOf(_zombieId) {
     Zombie storage myZombie = zombies[_zombieId];
     Zombie storage enemyZombie = zombies[_targetId];
     uint rand = randMod(100);
